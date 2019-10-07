afd96555c453d44069669abdfaf89131d4a40346
Firev1001 office-pc <fireb1001@gmail.com>
2019-10-07 20:40:55 +0200

    suparoi

--- public/electron.js
+++ public/electron.js
@@ -5,13 +5,14 @@ const path = require("path");
 const isDev = require("electron-is-dev");
 let mainWindow;
 function createWindow() {
-mainWindow = new BrowserWindow({ width: 900, height: 680 });
+mainWindow = new BrowserWindow();
 mainWindow.loadURL(
 isDev
 ? "http://localhost:3000"
 : `file://${path.join(__dirname, "../build/index.html")}`
 );
 mainWindow.on("closed", () => (mainWindow = null));
+mainWindow.maximize()
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
@@ -21,6 +22,6 @@ app.quit();
 });
 app.on("activate", () => {
 if (mainWindow === null) {
-createWindow();
+    createWindow();
 }
 });
