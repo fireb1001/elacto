430ad41334e7b0d059f75c4059d8d3d201c3a843
Firev1001 office-pc <fireb1001@gmail.com>
2019-10-07 21:20:26 +0200

    sofaro

--- public/electron.js
+++ public/electron.js
@@ -1,18 +1,28 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Tray = electron.Tray;
 const path = require("path");
 const isDev = require("electron-is-dev");
 let mainWindow;
+let tray;
+
 function createWindow() {
-mainWindow = new BrowserWindow();
+mainWindow = new BrowserWindow({ width: 900, height: 680 , frame: true});
 mainWindow.loadURL(
 isDev
 ? "http://localhost:3000"
 : `file://${path.join(__dirname, "../build/index.html")}`
 );
 mainWindow.on("closed", () => (mainWindow = null));
-mainWindow.maximize()
+tray= new Tray(path.join(__dirname,'./logo192.png'))
+tray.on('click', ()=>{
+    if(mainWindow.isVisible()) {
+        mainWindow.hide();
+    } else {
+        mainWindow.show();
+    }
+})
 }
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
@@ -22,6 +32,6 @@ app.quit();
 });
 app.on("activate", () => {
 if (mainWindow === null) {
-    createWindow();
+createWindow();
 }
 });
